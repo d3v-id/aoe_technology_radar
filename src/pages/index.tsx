41f55472ef1ab91229e857682bd1a9a0f15c9301
David <d.rueffreck@web.de>
2025-09-24 22:06:57 +0200

    fix: prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,108 +1,113 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+// import SplitText from "@/components/SplitText"
 import GooeyNav from "@/components/GooeyNav";
 import { QuadrantList } from "@/components/QuadrantList/QuadrantList";
 import { Radar } from "@/components/Radar/Radar";
-import SplitText from "@/components/SplitText";
 import { Tags } from "@/components/Tags/Tags";
 import {
-  getAppName,
-  getChartConfig,
-  getItems,
-  getLabel,
-  getQuadrants,
-  getReleases,
-  getRings,
-  getSections,
-  getTags,
-  getToggle,
+	getAppName,
+	getChartConfig,
+	getItems,
+	getLabel,
+	getQuadrants,
+	getReleases,
+	getRings,
+	getSections,
+	getTags,
+	getToggle,
 } from "@/lib/data";
 import { CustomPage } from "@/pages/_app";
 
 const Home: CustomPage = () => {
-  const router = useRouter();
-  const tag = router.query.tag as string | undefined;
-  const appName = getAppName();
-  const metaDescription = getLabel("metaDescription");
-  const chartConfig = getChartConfig();
-  const sections = getSections();
-  const version = getReleases().length;
-  const rings = getRings();
-  const quadrants = getQuadrants();
-  const tags = getTags();
-  const items = getItems(undefined, true).filter(
-    (item) => !tag || item.tags?.includes(tag),
-  );
-  const items2 = [
-    { label: "Home", href: "#" },
+	const router = useRouter();
+	const tag = router.query.tag as string | undefined;
+	const appName = getAppName();
+	const metaDescription = getLabel("metaDescription");
+	const chartConfig = getChartConfig();
+	const sections = getSections();
+	const version = getReleases().length;
+	const rings = getRings();
+	const quadrants = getQuadrants();
+	const tags = getTags();
+	const items = getItems(undefined, true).filter(
+		(item) => !tag || item.tags?.includes(tag),
+	);
 
-    { label: "About", href: "#" },
+	const items2 = [
+		{ label: "Home", href: "#" },
 
-    { label: "Contact", href: "#" },
-  ];
+		{ label: "About", href: "#" },
 
-  return (
-    <>
-      <Head>
-        {metaDescription && (
-          <meta name="description" content={metaDescription} />
-        )}
-      </Head>
+		{ label: "Contact", href: "#" },
+	];
 
-      <h1>
-        {appName}{" "}
-        {/* <span style={{ color: "var(--highlight)", whiteSpace: "nowrap" }}>
+	return (
+		<>
+			<Head>
+				{metaDescription && (
+					<meta name="description" content={metaDescription} />
+				)}
+			</Head>
+
+			<h1>
+				{appName}{" "}
+				{/* <span style={{ color: "var(--highlight)", whiteSpace: "nowrap" }}>
           Version #{version}
         </span> */}
-        <SplitText text="Hello World" className="text-3xl font-bold" />
-      </h1>
-      <div style={{ height: "600px", position: "relative" }}>
-        <GooeyNav
-          items={items2}
-          particleCount={15}
-          particleDistances={[90, 10]}
-          particleR={100}
-          initialActiveIndex={0}
-          animationTime={600}
-          timeVariance={300}
-          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-        />
-      </div>
+				{/* <SplitText 
+          text="Hello World" 
+          className="text-3xl font-bold" 
+        /> */}
+			</h1>
+			{/* <div style={{ height: '600px', position: 'relative' }}>
 
-      {sections.map((section) => {
-        switch (section) {
-          case "radar":
-            return (
-              getToggle("showChart") && (
-                <Radar
-                  key={section}
-                  size={chartConfig.size}
-                  quadrants={quadrants}
-                  rings={rings}
-                  items={items}
-                />
-              )
-            );
-          case "tags":
-            return (
-              getToggle("showTagFilter") &&
-              tags.length > 0 && (
-                <Tags key={section} tags={tags} activeTag={tag} />
-              )
-            );
-          case "list":
-            return (
-              getToggle("showQuadrantList") && (
-                <QuadrantList key={section} items={items} />
-              )
-            );
-          default:
-            return null;
-        }
-      })}
-    </>
-  );
+      <GooeyNav
+        items={items2}
+        particleCount={15}
+        particleDistances={[90, 10]}
+        particleR={100}
+        initialActiveIndex={0}
+        animationTime={600}
+        timeVariance={300}
+        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+      />
+      </div> */}
+
+			{sections.map((section) => {
+				switch (section) {
+					case "radar":
+						return (
+							getToggle("showChart") && (
+								<Radar
+									key={section}
+									size={chartConfig.size}
+									quadrants={quadrants}
+									rings={rings}
+									items={items}
+								/>
+							)
+						);
+					case "tags":
+						return (
+							getToggle("showTagFilter") &&
+							tags.length > 0 && (
+								<Tags key={section} tags={tags} activeTag={tag} />
+							)
+						);
+					case "list":
+						return (
+							getToggle("showQuadrantList") && (
+								<QuadrantList key={section} items={items} />
+							)
+						);
+					default:
+						return null;
+				}
+			})}
+		</>
+	);
 };
 
 export default Home;
